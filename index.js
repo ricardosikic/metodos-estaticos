f57ec688f52863b5a0b2e405dbabcfde229cea39
ricardosikic <ricardo.sikic@gmail.com>
2020-01-22 13:18:28 +0000

    otro exercise

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ class User {
 console.log(User.method_one());
 
 // los metodos estaticos son usados para implementar funciones que pertenecen a una clase pero no a un objeto en particular.
+
+
+class Auto {
+   constructor(brand, year, doors) {
+     this.on = false;
+     this.brand = brand;
+     this.year = year;
+     this.doors = doors || 4;
+   }
+
+   static turningOn() {
+     setTimeout(() => {
+       this.on = true;
+       let alert = '';
+       this.on ? alert = 'on' : '';
+       console.log(`car is ${alert}`);
+     }, 3000);
+   }
+}
+
+// Lo que se ve aca es que el metodo de la clase se define como estatico por lo que no hay necesidad de instaciar un objeto para poder acceder a el. 
+
+console.log(Auto.turningOn());
